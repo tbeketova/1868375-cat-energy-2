ce65f934e83b620339fa41df61820587dbccf562
Tatiana B <tbeketova04@gmail.com>
2024-10-01 21:18:33 +0300

    добавляет js код для бургер-меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,13 @@
 /* в этот файл добавляет скрипты*/
+
+//Находим кнопку бургер-меню и навигационное меню
+document.addEventListener('DOMContentLoaded', function () {
+  const buttonToggle = document.querySelector('.navigation__toggle');
+  const nav = document.querySelector('.navigation');
+
+    // При нажатии на кнопку добавляем или удаляем класс 'navigation--opened' и 'navigation--closed'
+  buttonToggle.addEventListener('click', function () {
+    nav.classList.toggle('navigation--opened');
+    nav.classList.toggle('navigation--closed');
+  });
+});
